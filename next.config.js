1fff0010fff5e7c5fbf80bb4d39616c4b4a98989
woosang <woosang3078@hanmail.net>
2023-08-29 11:23:24 +0900

    feat: 프리미어리그 뉴스 API 추가

--- next.config.js
+++ next.config.js
@@ -3,9 +3,23 @@ const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
-      {
+      { 
+        // 프리미어리그 순위
         source: "/api/pl/rank",
-        destination: `https://api.football-data.org/v4/competitions/PL/standings`,
+        destination:
+          "https://api.football-data.org/v4/competitions/PL/standings",
+      },
+      {
+        // 프리미어리그 득점왕
+        source: "/api/pl/scorer",
+        destination: "https://api.football-data.org/v4/competitions/PL/scorers",
+      },
+      {
+        // 프리미어리그 뉴스
+        source: "/api/pl/news",
+        destination: encodeURI(
+          "https://openapi.naver.com/v1/search/news.json?query=%ED%94%84%EB%A6%AC%EB%AF%B8%EC%96%B4%EB%A6%AC%EA%B7%B8&display=10&start=1&sort=sim&startDate=20230801&endDate=20230820"
+        ),
       },
     ];
   },
