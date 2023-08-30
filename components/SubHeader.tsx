165a6538ac050959ce8e62c8af3dc27073eef8eb
woosang <woosang3078@hanmail.net>
2023-08-30 10:19:39 +0900

    docs: 카테고리 URL 수정

--- components/SubHeader.tsx
+++ components/SubHeader.tsx
@@ -1,37 +1,73 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import Link from "next/link";
 
 interface SubHeaderProps {
-    league: string;
+  league: string;
 }
-
 
 const SubHeader = ({ league }: SubHeaderProps) => {
-    return (
-        <>
-          <HeaderContainer league={league}>
-            <CategoryContainer>
-                <Categories>리그 순위</Categories>
-                <Categories>득점왕</Categories>
-                <Categories>리그 소식</Categories>
-            </CategoryContainer>
-          </HeaderContainer>
-        </>
-    )
-}
+  return (
+    <>
+      <HeaderContainer league={league}>
+        <CategoryContainer>
+          <Categories>
+            <LinkStyle href={`/${league}/rank`} league={league}>
+              리그 순위
+            </LinkStyle>
+          </Categories>
+          <Categories>
+            <LinkStyle href={`/${league}/scorer`} league={league}>
+              득점왕
+            </LinkStyle>
+          </Categories>
+          <Categories>
+            <LinkStyle href={`/${league}/news`} league={league}>
+              리그 소식
+            </LinkStyle>
+          </Categories>
+        </CategoryContainer>
+      </HeaderContainer>
+    </>
+  );
+};
 
 export default SubHeader;
 
+const LinkStyle = styled(Link)<SubHeaderProps>`
+  text-decoration: none;
+  cursor: pointer;
+  color: ${(props) =>
+    props.league === "premier"
+      ? "black"
+      : props.league === "bundes"
+      ? "white"
+      : "black"};
+  font-size: 19px;
+`;
+
 const Categories = styled.li`
-    
-`
+  margin-right: 40px;
+`;
 
 const CategoryContainer = styled.ul`
-    
-`
+  display: flex;
+  list-style: none;
+  margin-left: auto;
+  margin-right: 100px;
+`;
 
 const HeaderContainer = styled.nav<SubHeaderProps>`
   width: 100%;
   height: 90px;
-  background-image: ${(props) => (props.league === "premier" ? "url('/premier_header.svg')" : "url('../assets/svgs/league.svg')")};
+  background-image: ${(props) =>
+    props.league === "premier"
+      ? "url('/premier_header.svg')"
+      : props.league === "bundes"
+      ? "url('/bundes_header.svg')"
+      : "url('../assets/svgs/league.svg')"};
   background-size: cover;
-`
+  display: flex;
+  align-items: center;
+  position: sticky;
+  top: 0px;
+`;
