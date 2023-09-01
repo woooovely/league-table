efa1af9577a5b208dd2c74c3dd77a97014090ea8
woosang <woosang3078@hanmail.net>
2023-09-01 13:31:32 +0900

    feat: 경고문 컴포넌트 추가

---
+++ components/alert.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Alert = () => {
+    return (
+        <>
+          
+        </>
+    )
+}
+
+export default Alert;
+
+const Background = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: black;
+`
