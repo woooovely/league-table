9c7e995c158a81e362e34c12dbd1a44c3fb3a73d
woooovely <woosang3078@dsm.hs.kr>
2024-10-22 10:10:21 +0900

    fix: 해외축구 시즌 변경으로 인한 리그 로고 수정

--- next.config.js
+++ next.config.js
@@ -1,29 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        // 리그1 순위
-        source: "/api/league/rank",
-        destination:
-          "https://api.football-data.org/v4/competitions/FL1/standings",
-      },
-      {
-        // 리그1 득점왕
-        source: "/api/league/scorer",
-        destination:
-          "https://api.football-data.org/v4/competitions/FL1/scorers",
-      },
-      {
-        // 리그1 뉴스
-        source: "/api/league/news",
-        destination: encodeURI(
-          "https://openapi.naver.com/v1/search/news.json?query=%EB%A6%AC%EA%B7%B8%EC%95%99&display=10&start=1&sort=sim&startDate=20230801&endDate=20230820"
-        ),
-      },
-    ];
-  },
+  compiler: {
+    styledComponents: true,
+  }
 };
 
 module.exports = nextConfig;
