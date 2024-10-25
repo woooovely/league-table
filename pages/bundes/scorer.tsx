aaf3577dc60878d0957c65b8b2e5510970928b5a
woooovely <woosang3078@dsm.hs.kr>
2024-10-25 14:33:54 +0900

    refactor: 분데스리가 득점왕 코드 리팩토링

--- pages/bundes/scorer.tsx
+++ pages/bundes/scorer.tsx
@@ -8,28 +8,8 @@ import {
   bundesTeamName,
   nationality,
 } from "@/constants/constants";
-
-interface CompetitionProps {
-  id: number;
-  name: string;
-  code: string;
-  emblem: string;
-}
-
-interface Scorer {
-  player: {
-    id: number;
-    name: string;
-    nationality: string;
-  };
-  team: {
-    id: number;
-    name: string;
-    crest: string;
-    website: string;
-  };
-  goals: number;
-}
+import { CompetitionProps, Scorer } from "@/types/scorer-type";
+import ScorerRank from "@/components/ScorerRank/ScorerRank";
 
 interface ScorerDataProps {
   scorers: Scorer[];
@@ -44,32 +24,7 @@ const BundesScorer = ({ scorers, competition }: ScorerDataProps) => {
       </Head>
       <MainHeader />
       <SubHeader league="bundes" />
-      <S.LogoContainer>
-        <img src={competition.emblem} alt="리그 로고" />
-      </S.LogoContainer>
-      <S.BoxContainer>
-        <S.TableTitle>
-          <S.Lists>순위</S.Lists>
-          <S.Lists>선수명</S.Lists>
-          <S.Lists>득점</S.Lists>
-          <S.Lists>소속팀</S.Lists>
-          <S.Lists>국적</S.Lists>
-        </S.TableTitle>
-        {scorers.map((item: Scorer, index: number) => (
-          <S.Container key={item.player.id}>
-            <S.Table>
-              <S.Rank>{index + 1}</S.Rank>
-              <S.Player>{bundesPlayerName[item.player.name]}</S.Player>
-              <S.Goals>{item.goals}</S.Goals>
-              <S.TeamContainer>
-                <S.TeamLogo src={item.team.crest} alt="팀 로고" />
-                {bundesTeamName[item.team.name]}
-              </S.TeamContainer>
-              <S.Nation>{nationality[item.player.nationality]}</S.Nation>
-            </S.Table>
-          </S.Container>
-        ))}
-      </S.BoxContainer>
+      <ScorerRank scorers={scorers} competition={competition} />
     </div>
   );
 };
