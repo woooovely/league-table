5d83b6b9c79ef256d48cc3a8660c48b20d94bff0
woooovely <woosang3078@dsm.hs.kr>
2024-10-25 15:05:18 +0900

    refactor: 리그1 득점왕 코드 리팩토링

--- pages/league/scorer.tsx
+++ pages/league/scorer.tsx
@@ -2,34 +2,8 @@ import Head from "next/head";
 import axios from "axios";
 import MainHeader from "@/components/MainHeader";
 import SubHeader from "@/components/SubHeader";
-import * as S from "@/styles/league/scorer";
-import {
-  leaguePlayerName,
-  leagueTeamName,
-  nationality,
-} from "@/constants/constants";
-
-interface CompetitionProps {
-  id: number;
-  name: string;
-  code: string;
-  emblem: string;
-}
-
-interface Scorer {
-  player: {
-    id: number;
-    name: string;
-    nationality: string;
-  };
-  team: {
-    id: number;
-    name: string;
-    crest: string;
-    website: string;
-  };
-  goals: number;
-}
+import { CompetitionProps, Scorer } from "@/types/scorer-type";
+import ScorerRank from "@/components/ScorerRank/ScorerRank";
 
 interface ScorerProps {
   scorers: Scorer[];
@@ -44,32 +18,7 @@ const LeagueScorer = ({ scorers, competition }: ScorerProps) => {
       </Head>
       <MainHeader />
       <SubHeader league="league" />
-      <S.LogoContainer>
-        <img src={competition.emblem} alt="리그 로고" />
-      </S.LogoContainer>
-      <S.BoxContainer>
-        <S.TableTitle>
-          <S.Lists>순위</S.Lists>
-          <S.Lists>선수명</S.Lists>
-          <S.Lists>득점</S.Lists>
-          <S.Lists>소속팀</S.Lists>
-          <S.Lists>국적</S.Lists>
-        </S.TableTitle>
-        {scorers.map((item: Scorer, index: number) => (
-          <S.Container key={item.player.id}>
-            <S.Table>
-              <S.Rank>{index + 1}</S.Rank>
-              <S.Player>{leaguePlayerName[item.player.name]}</S.Player>
-              <S.Goals>{item.goals}</S.Goals>
-              <S.TeamContainer>
-                <S.TeamLogo src={item.team.crest} alt="팀 로고" />
-                {leagueTeamName[item.team.name]}
-              </S.TeamContainer>
-              <S.Nation>{nationality[item.player.nationality]}</S.Nation>
-            </S.Table>
-          </S.Container>
-        ))}
-      </S.BoxContainer>
+      <ScorerRank scorers={scorers} competition={competition} />
     </div>
   );
 };
